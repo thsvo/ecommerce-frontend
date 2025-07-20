f003a39fbfe4924da75591c3a6097317352c83ed
thsvo <155262940+thsvo@users.noreply.github.com>
2025-07-20 15:39:44 +0600

    build time error solve

--- src/app/api/payment-intent/route.ts
+++ src/app/api/payment-intent/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-06-30.basil',
 });
 
 import { NextRequest } from 'next/server';
